876fc2df2374a59a3d3296d77d4d0a3a3295820e
maxsobolev97 <77441161+maxsobolev97@users.noreply.github.com>
2022-03-24 13:14:43 +0300

    Add files via upload

---
+++ QUIK_CLIENT.ps1
@@ -0,0 +1,83 @@
+﻿Import-Module Posh-SSH
+
+$IN_Z = "Z:\example\"
+$ARC_Z = "Z:\example\"
+$IN_V = "V:\example\"
+$ARC_V = "V:\example\arc\"
+
+while($true){
+
+    Write-Host("$(Get-Date)    Открываю сессию к example")
+    $User = "example"
+    $PWord = ConvertTo-SecureString -String "example" -AsPlainText -Force
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+    New-SFTPSession -ComputerName *ip* -Credential ($Credential) -Verbose | fl
+    Get-SFTPSession -SessionId 0
+
+    if(Test-SFTPPath -Path /home/example/example/example/example/example/example/example/ -SessionId 0){
+
+        $files = Get-SFTPChildItem -SessionId 0 -Path /home/example/example/example/example/example/example/example/
+    
+        foreach($file in $files){
+    
+            if($file.FullName -like "*.xml"){
+
+                Write-Host("$(Get-Date)    Копирование файлов")
+
+                Get-SFTPFile -LocalPath $IN_Z -RemoteFile $file.FullName -SessionId 0 -Overwrite
+                Get-SFTPFile -LocalPath $ARC_Z -RemoteFile $file.FullName -SessionId 0 -Overwrite
+                Remove-SFTPItem -Path $file.FullName -SessionId 0 -Force
+
+            }
+
+        }
+
+    }
+
+    if(Test-SFTPPath -Path /example/example/example/example/example/example/example/example/example/ -SessionId 0){
+
+        $files = Get-SFTPChildItem -SessionId 0 -Path /example/example/example/example/example/example/example/example/example/
+    
+        foreach($file in $files){
+
+                if($file.FullName -like "*.sql"){
+            
+                    Write-Host("$(Get-Date)    Попытка копирование файлов")
+
+                    $files_V = Get-ChildItem $IN_V -Filter "*.sql"
+                
+                    if($files_V -eq $null){
+                
+                        $fileName = $file.FullName
+                        Write-Host("$(Get-Date)    Перенос файла $fileName")
+                        Get-SFTPFile -LocalPath $IN_V -RemoteFile $file.FullName -SessionId 0 -Overwrite
+                        $file_sql = Get-ChildItem $IN_V -Filter '*.sql'
+                        Rename-Item -Path $file_sql.FullName -NewName "TTT0.sql"
+                        Get-SFTPFile -LocalPath $ARC_V -RemoteFile $file.FullName -SessionId 0 -Overwrite
+                        Write-Host("$(Get-Date)    Удаление перенесенного файла")
+                        Remove-SFTPItem -Path $file.FullName -SessionId 0 -Force                
+                
+                    } else {
+                
+                        Write-Host("$(Get-Date)    Обнаружен не загруженный файл *.sql")
+                
+                    }
+            
+            }
+            
+        }
+        
+    }
+
+    
+
+    Write-Host("$(Get-Date)    Закрываю сессию к example")
+    Remove-SFTPSession -SessionId 0
+
+    [System.GC]::Collect()
+
+    Write-Host("$(Get-Date)    Пауза 1 минута")
+    Start-Sleep 60
+
+}
+
---
+++ QUIK_LIMITS.ps1
@@ -0,0 +1,56 @@
+﻿Import-Module Posh-SSH
+
+$IN_X = "X:\"
+$ARC_X = "X:\arc\"
+
+while($true){
+
+    Write-Host("$(Get-Date)    Открываю сессию к example")
+
+    $User = "example"
+    $PWord = ConvertTo-SecureString -String "example" -AsPlainText -Force
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+    New-SFTPSession -ComputerName 172.16.100.18 -Credential ($Credential) -Verbose | fl
+    Get-SFTPSession -SessionId 0
+
+    if(Test-SFTPPath -Path /home/example/example/example/example/example/example/example/ -SessionId 0){
+        Write-Host "Поиск файлов"
+        $files = Get-SFTPChildItem -SessionId 0 -Path /home/example/example/example/example/example/example/example/
+    
+        foreach($file in $files){
+    
+            $correctLCI = Get-ChildItem $IN_X -Filter "Correct.lci"
+
+            if($correctLCI -eq $null){
+
+                if($file.FullName -like "*.lci"){
+                    $fileName = $file.FullName
+                    Write-Host("$(Get-Date)    Перенос файла $fileName")
+                    Get-SFTPFile -LocalPath $IN_X -RemoteFile $file.FullName -SessionId 0 -Overwrite
+                    $file_lci = Get-ChildItem $IN_X -Filter '*.lci'
+                    Rename-Item -Path $file_lci.FullName -NewName "Correct.lci"
+                    Get-SFTPFile -LocalPath $ARC_X -RemoteFile $file.FullName -SessionId 0 -Overwrite
+                    Write-Host("$(Get-Date)    Удаление перенесенного файла")
+                    Remove-SFTPItem -Path $file.FullName -SessionId 0 -Force
+
+                }
+
+            } else {
+                Write-Host("$(Get-Date)    Обнаружен не забранный файл")
+                Start-Sleep 5
+        
+            }
+
+        }
+
+    }
+    Write-Host("$(Get-Date)    Закрываю сессию к example")
+    Remove-SFTPSession -SessionId 0
+    Write-Host("$(Get-Date)    Пауза 30 секунд")
+    [System.GC]::Collect()
+    Start-Sleep 30
+
+}
+
+
+
